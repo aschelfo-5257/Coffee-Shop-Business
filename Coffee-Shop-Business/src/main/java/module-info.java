6f76652d0a9f7dea012f7425268b747f72a6f3e3
Aaron Schelfo <schelfoaaron918@gmail.com>
2026-07-01 20:29:21 -0500

    module-info.java

---
+++ Coffee-Shop-Business/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module coffee.shop.business {
+    // Requires standard JDK desktop/logging tools if needed
+    requires java.logging;
+
+    // Only add exports/opens once those specific package folders physically exist
+}
